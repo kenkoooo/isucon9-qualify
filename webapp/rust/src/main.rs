8e825767341ac07f08f556bd4c06fe76ea6d49d4
kenkoooo <kenkou.n@gmail.com>
2020-07-14 20:49:17 +0900

    .gitignore

---
+++ webapp/rust/src/main.rs
@@ -0,0 +1,14 @@
+use actix_web::{web, App, HttpServer, Responder};
+
+#[get("/{id}/{name}/index.html")]
+async fn index(info: web::Path<(u32, String)>) -> impl Responder {
+    format!("Hello {}! id:{}", info.1, info.0)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind("0.0.0.0:8080")?
+        .run()
+        .await
+}
